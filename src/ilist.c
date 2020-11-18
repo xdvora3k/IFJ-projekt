6635053746c920e83ed6ace2899e74a0e0d440fd
xkvasn14 <xkvasn14@vutbr.cz>
2020-11-18 17:11:12 +0100

    Revert "setting up branch"
    This reverts commit f2598e0b59a8cade39eae5cb12166dcbc57395b3.

--- src/ilist.c
+++ src/ilist.c
@@ -5,96 +5,116 @@
 
 #include "ilist.h"
 
-void InstrLLInit(tLinkedList *L){
+void LLInit(tListOfInstr *L){
+    L->active = NULL;
     L->first = NULL;
+    L->last = NULL;
 }
 
-void StrLLInit(tLinkedList *L){
-    L->first = NULL;
-}
-
-void InstrLLDisposeAll(tLinkedList *L){
+void LLDisposeAll(tListOfInstr *L){
     while (L->first){
         tListItem *to_delete = L->first;
         L->first = L->first->nextItem;
         free(to_delete);
     }
+    L->active = NULL;
     L->first = NULL;
+    L->last = NULL;
 }
 
-void InstrLLInsertFirst(tLinkedList *L, tInstr* Instruction){
+void LLInsertFirst(tListOfInstr *L, tInstr Instruction){
     tListItem *new_node = (tListItem*) malloc(sizeof(tListItem));
     new_node->nextItem = L->first;
-    new_node->Content = Instruction;
+    new_node->Instruction = Instruction;
+    if (!L->first || !L->first->nextItem){
+        L->last = new_node;
+    }
     L->first = new_node;
 }
 
-void StrLLInsert(tLinkedList *L, string *K){
-    tListItem *new_node = (tListItem*) malloc(sizeof(tListItem));
-    new_node->Content = K;
-    if (!L->first){
-        L->first = new_node;
-        return;
-    }
-    tListItem *curr = L->first;
-    while (curr->nextItem){
-        curr = curr->nextItem;
-    }
-    curr->nextItem = new_node;
+void LLSetActiveToFirst(tListOfInstr *L){
+    L->active = L->first;
 }
 
-void InstrLLDeleteFirst(tLinkedList *L){
+void LLSetActiveToLast(tListOfInstr *L){
+    L->active = L->last;
+}
+
+void LLCopyFirst(tListOfInstr *L, tInstr *Instruction){
+    if (!L->first){
+        return;
+    }
+    *Instruction = L->first->Instruction;
+}
+
+void LLDeleteFirst(tListOfInstr *L){
     if (!L->first){
         return;
     }
     tListItem *to_delete = L->first;
     L->first = L->first->nextItem;
+    if (L->active == to_delete){
+        L->active = NULL;
+    }
     free(to_delete);
 }
 
-int StrLLStringAlreadyOccupied(tLinkedList *L, char *S){
-    tListItem *node = L->first;
-    while (node){
-        if (!strcmp(((string*) node->Content)->str, S)){
-            return TRUE;
-        }
-        node = node->nextItem;
-    }
-    return FALSE;
-}
-
-void StrLLDeleteLast(tLinkedList *L){
-    tListItem **to_delete = &L->first;
-    if (!to_delete){
+void LLPostDelete(tListOfInstr *L){
+    if (!L->active || !L->active->nextItem){
         return;
     }
-    while ((*to_delete)->nextItem){
-        to_delete = &(*to_delete)->nextItem;
+    if (L->last == L->active->nextItem){
+        L->last = L->active;
     }
-    (*to_delete)->nextItem = NULL;
-    free(*to_delete);
+    tListItem *to_delete = L->active->nextItem;
+    L->active->nextItem = L->active->nextItem->nextItem;
+    free(&to_delete->Instruction);
+    free(to_delete);
 }
 
-void StrLLDispose(tLinkedList *L){
-    while (L->first){
-        StrLLDeleteLast(L);
+void LLPostInsert(tListOfInstr *L, tInstr Instruction){
+    if (!L->active){
+        return;
     }
+    tListItem *new_node = (tListItem*) malloc(sizeof(tListItem));
+    new_node->Instruction = Instruction;
+    tListItem *to_move = L->active->nextItem;
+    new_node->nextItem = to_move;
+    if (!L->active->nextItem){
+        L->last = new_node;
+    }
+    L->active->nextItem = new_node;
 }
 
-tListItem* StrLLLocateNthElem(tLinkedList *L, int index){
-    tListItem *item = L->first;
-    for (int i = 0; i < index; i++){
-        item = item->nextItem;
+void LLInsertLast(tListOfInstr *L, tInstr Instruction){
+    tListItem *new_node = (tListItem*) malloc(sizeof(tListItem));
+    new_node->Instruction = Instruction;
+    if (!L->last){
+        L->first = new_node;
+        L->last = new_node;
+        return;
     }
-    return item;
+    L->last->nextItem = new_node;
+    L->last = new_node;
 }
 
-int StrLLLen(tLinkedList *L){
-    int i = 0;
-    tListItem *item = L->first;
-    while (item){
-        i++;
-        item = item->nextItem;
+void LLCopyActive(tListOfInstr *L, tInstr *Instruction){
+    if (!L->active){
+        return;
     }
-    return i;
+    *Instruction = L->active->Instruction;
+}
+
+void LLActualize(tListOfInstr *L, tInstr Instruction){
+    if (!L->active){
+        return;
+    }
+    L->active->Instruction = Instruction;
+}
+
+void LLActiveNext(tListOfInstr *L){
+    if (!L->active){
+        return;
+    }
+    L->active = L->active->nextItem;
 }
--- src/ilist.h
+++ src/ilist.h
@@ -8,10 +8,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "str.h"
-#include <string.h>
 
-// TODO: Fix Instr
+// TODO: Define types of instructions
+// Example:
+#define I_FUNC 0
+#define I_PLUS 1
+#define I_MINUS 2
+#define I_TIMES 3
+#define I_DIVIDE 4
+#define I_EQUAL 5
+#define I_ANOTHER 6
+// Once this is fixed, the test script needs to be fixed as well.
+
 typedef struct {
     int instType;
     void *addr1;
@@ -20,23 +28,28 @@ typedef struct {
 } tInstr;
 
 typedef struct listItem{
-    void* Content;
+    tInstr Instruction;
     struct listItem *nextItem;
 } tListItem;
 
 typedef struct {
     struct listItem *first;
-} tLinkedList;
+    struct listItem *last;
+    struct listItem *active;
+} tListOfInstr;
 
-void InstrLLInit(tLinkedList *L);
-void InstrLLDisposeAll(tLinkedList *L);
-void InstrLLInsertFirst(tLinkedList *L, tInstr *Instruction);
-void InstrLLDeleteFirst(tLinkedList *L);
-
-void StrLLInit(tLinkedList *L);
-void StrLLInsert(tLinkedList *L, string *K);
-int StrLLStringAlreadyOccupied(tLinkedList *L, char *S);
-tListItem* StrLLLocateNthElem(tLinkedList *L, int index);
-int StrLLLen(tLinkedList *L);
+void LLInit(tListOfInstr *L);
+void LLDisposeAll(tListOfInstr *L);
+void LLInsertFirst(tListOfInstr *L, tInstr Instruction);
+void LLSetActiveToFirst(tListOfInstr *L);
+void LLSetActiveToLast(tListOfInstr *L);
+void LLCopyFirst(tListOfInstr *L, tInstr *Instruction);
+void LLDeleteFirst(tListOfInstr *L);
+void LLPostDelete(tListOfInstr *L);
+void LLPostInsert(tListOfInstr *L, tInstr Instruction);
+void LLInsertLast(tListOfInstr *L, tInstr Instruction);
+void LLCopyActive(tListOfInstr *L, tInstr *Instruction);
+void LLActualize(tListOfInstr *L, tInstr Instruction);
+void LLActiveNext(tListOfInstr *L);
 
 #endif
