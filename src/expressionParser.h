7b39803c5722123b0bccc7cc40885e2987640924
MarieKz <ma.kuzelova@centrum.cz>
2020-12-08 00:58:57 +0100

    updated expressionparser

--- src/expressionParser.h
+++ src/expressionParser.h
@@ -1,3 +1,8 @@
+/*
+ * IFJ project 2020
+ * Author: xkuzel08, Marie Kuzelova
+ *         
+ */
 #ifndef IFJ_PROJEKT_EXPRESSION_PARSER_H
 #define IFJ_PROJEKT_EXPRESSION_PARSER_H
 
@@ -5,8 +10,8 @@
 #include "ilist.h"
 
 
-tLinkedList* get_tokens(string *s);
-void get_tokenExp(tToken* token, string *input, int startIndex);
+tLinkedList* get_tokens(string *s, tLinkedList *List);
+void get_tokenExp(tToken* token, string *input, int startIndex, tLinkedList *L);
 void _save_to_token(tToken *token, string *string, tState type, int end_index, tLinkedList *list);
 tVarDataType getDataType(tToken *token, tLinkedList *linkedL);
 #endif
